a0db47471e9bd130e44be4db5697be9c0169d182
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-01-05 21:13:26 -0800

    wip ci push back

--- tools/push_github.py
+++ tools/push_github.py
@@ -1,6 +1,6 @@
 
 import os
-from subprocess import check_call
+from subprocess import check_call, CalledProcessError
 
 
 AUTOCOMMIT_LOGIN = os.getenv('GH_TOKEN', '')
@@ -10,27 +10,38 @@ AUTOCOMMIT_BRANCH = os.getenv('AUTOCOMMIT_BRANCH', os.getenv('TRAVIS_BRANCH'))
 AUTOCOMMIT_TARGET = os.getenv('AUTOCOMMIT_TARGET', '.')
 AUTOCOMMIT_PREFIX = os.getenv('AUTOCOMMIT_PREFIX', 'CI autocommit')
 
-'''TRAVIS_JOB_WEB_URL'''
 
 def call(args):
-    print(args)
+    args = [unicode(a) for a in args]
+    print(['$'] + args)
     ret = check_call(args)
     print(ret)
     return ret
 
 
+def checkout_or_create(branch_name):
+    try:
+        return call(['git', 'checkout', branch_name])
+    except CalledProcessError:
+        return call(['git', 'checkout', '-b', branch_name])
+
+
 def main():
+    branch_name = AUTOCOMMIT_BRANCH
+    if not branch_name:
+        raise RuntimeError('expected AUTOCOMMIT_BRANCH env var to be set')
     call(['git', 'config', '--global', 'user.email', AUTOCOMMIT_EMAIL])
     call(['git', 'config', '--global', 'user.name', AUTOCOMMIT_NAME])
 
-    # TODO: check which remote/branch are default
-    # call(['git', 'checkout', '-b', AUTOCOMMIT_BRANCH])
+    # see default remotes
+    call(['git', 'remote', '-v'])
+    checkout_or_create(branch_name)
 
     call(['git', 'add', AUTOCOMMIT_TARGET])
     call(['git', 'commit', '--message', AUTOCOMMIT_PREFIX + ': ' + os.getenv('TRAVIS_JOB_WEB_URL')])
 
     call(['git', 'remote', 'add', 'origin-autocommit', 'https://%s@github.com/mahmoud/zerover.git' % AUTOCOMMIT_LOGIN])  # TODO
-    call(['git', 'push', '--set-upstream', 'origin-autocommit', AUTOCOMMIT_BRANCH])
+    call(['git', 'push', '--set-upstream', 'origin-autocommit', branch_name])
 
     return
 
