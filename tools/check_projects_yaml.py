ed5819c08948926ac808f960ba5669bc553701d5
Matthew Vine <mattvine03@gmail.com>
2024-12-28 17:19:23 -0500

    feat: better schema checks

--- tools/check_projects_yaml.py
+++ tools/check_projects_yaml.py
@@ -19,15 +19,6 @@ OPTIONAL = {
     Optional("emeritus"): bool,
     Optional("reason"): str,
     Optional("star_count"): int,
-    Optional("release_count"): int,
-    Optional("release_count_zv"): int,
-    Optional("latest_release_date"): Or(datetime.date, datetime.datetime),
-    Optional("latest_release_version"): Or(float, str),
-    Optional("first_release_date"): Or(datetime.date, datetime.datetime),
-    Optional("first_release_version"): Or(float, str),
-    Optional("first_nonzv_release_date"): Or(datetime.date, datetime.datetime),
-    Optional("first_nonzv_release_version"): Or(float, str),
-    Optional("last_zv_release_version"): Or(float, str),
 }
 IN_SCHEMA = Schema(
     {
@@ -38,14 +29,67 @@ IN_SCHEMA = Schema(
                     **OPTIONAL,
                     "name": str,
                     "gh_url": check_url,
+                    Optional("emeritus"): False,
                     Optional("url"): check_url,  # Overrides gh_url for the hyperlink
+                    Optional("release_count"): int,
+                    Optional("latest_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("latest_release_version"): Or(float, str),
+                    Optional("first_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("first_release_version"): Or(float, str),
+                },  # type: ignore
+                # Emeritus GitHub projects
+                {
+                    **OPTIONAL,
+                    "name": str,
+                    "gh_url": check_url,
+                    "emeritus": True,
+                    Optional("url"): check_url,  # Overrides gh_url for the hyperlink
+                    Optional("release_count_zv"): int,
+                    Optional("first_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("first_release_version"): Or(float, str),
+                    Optional("first_nonzv_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("first_nonzv_release_version"): Or(float, str),
+                    Optional("last_zv_release_version"): Or(float, str),
                 },  # type: ignore
                 # Non-GitHub projects
                 {
                     **OPTIONAL,
                     "name": str,
                     "url": check_url,
+                    Optional("emeritus"): False,
+                    Optional("release_count"): int,
                     "first_release_date": Or(datetime.date, datetime.datetime),
+                    Optional("latest_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("latest_release_version"): Or(float, str),
+                    Optional("first_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("first_release_version"): Or(float, str),
+                },  # type: ignore
+                # Emeritus Non-GitHub projects
+                {
+                    **OPTIONAL,
+                    "name": str,
+                    "url": check_url,
+                    "emeritus": True,
+                    Optional("release_count_zv"): int,
+                    "first_release_date": Or(datetime.date, datetime.datetime),
+                    Optional("first_release_version"): Or(float, str),
+                    Optional("first_nonzv_release_date"): Or(
+                        datetime.date, datetime.datetime
+                    ),
+                    Optional("first_nonzv_release_version"): Or(float, str),
+                    Optional("last_zv_release_version"): Or(float, str),
                 },  # type: ignore
             )
         ],
